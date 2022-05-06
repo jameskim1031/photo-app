3a0ab698f59c550dfd592d4a43a45b92241053d0
jameskim1031 <jameskim2020@u.northwestern.edu>
2022-05-06 17:53:32 -0500

    first commit

---
+++ views/bookmarks.py
@@ -0,0 +1,97 @@
+from flask import Response, request
+from flask_restful import Resource
+from models import Bookmark, db, Post
+import json
+from views import get_authorized_user_ids
+
+class BookmarksListEndpoint(Resource):
+
+    def __init__(self, current_user):
+        self.current_user = current_user
+    
+    def get(self):
+        # get all bookmarks owned by the current user
+        bookmarks = Bookmark.query.filter_by(user_id=self.current_user.id)
+        print(bookmarks)
+        return Response(json.dumps([bookmark.to_dict() for bookmark in bookmarks]), mimetype="application/json", status=200)
+
+    def post(self):
+        # create a new "bookmark" based on the data posted in the body 
+        body = request.get_json()
+
+        #checks if post_id is valid format
+        try:
+            post_id = int(body.get('post_id'))
+        except:
+            return Response(json.dumps({"message":"'post_id' is not an int."}), mimetype="application/json",  status=400) 
+        
+        #checks if post_id is valid
+        if post_id >= len(Post.query.all()):
+            return Response(json.dumps({"message":"'post_id' does not exist."}), mimetype="application/json",  status=404) 
+
+        # checks if post is reachable (if its a post from someone I follow)
+        user_ids = get_authorized_user_ids(self.current_user)
+        posts = Post.query.filter(Post.user_id.in_(user_ids)).all()
+        if Post.query.filter_by(id=post_id).first() not in posts:
+            return Response(json.dumps({"message":"'post_id' is not reachable."}), mimetype="application/json",  status=404) 
+
+        # checks if I already have the post in bookmark
+        bookmarks = Bookmark.query.filter_by(user_id=self.current_user.id).all()
+        post_ids = [bookmark.post_id for bookmark in bookmarks]
+        if post_id in post_ids:
+            return Response(json.dumps({"message":"'post_id' is a duplicate."}), mimetype="application/json",  status=400) 
+        
+        
+        new_bookmark = Bookmark(
+            post_id= body.get('post_id'),
+            user_id= self.current_user.id
+        )
+
+        db.session.add(new_bookmark)    # issues the insert statement
+        db.session.commit()         # commits the change to the database 
+
+        return Response(json.dumps(new_bookmark.to_dict()), mimetype="application/json", status=201)
+
+class BookmarkDetailEndpoint(Resource):
+
+    def __init__(self, current_user):
+        self.current_user = current_user
+    
+    def delete(self, id):
+        # delete "bookmark" record where "id"=id
+        print(id)
+        try:
+            id = int(id)
+        except:
+            return Response(json.dumps({"message":"'id' is not an int."}), mimetype="application/json",  status=400)
+
+        if id >= len(Bookmark.query.all()):
+            return Response(json.dumps({"message":"'id' does not exist."}), mimetype="application/json",  status=404) 
+        
+        # gets ids of all the following + self
+        user_ids = get_authorized_user_ids(self.current_user)
+        #print(user_ids)
+        if Bookmark.query.get(id).user_id not in user_ids:
+            return Response(json.dumps({"message":"'id' is not authorized."}), mimetype="application/json",  status=404) 
+
+        bookmark = Bookmark.query.get(id)
+        Bookmark.query.filter_by(id=id).delete()
+        db.session.commit()
+        return Response(json.dumps({"message":"Bookmark id={0} was successfully deleted.".format(id)}), mimetype="application/json", status=200)
+
+
+
+def initialize_routes(api):
+    api.add_resource(
+        BookmarksListEndpoint, 
+        '/api/bookmarks', 
+        '/api/bookmarks/', 
+        resource_class_kwargs={'current_user': api.app.current_user}
+    )
+
+    api.add_resource(
+        BookmarkDetailEndpoint, 
+        '/api/bookmarks/<int:id>', 
+        '/api/bookmarks/<int:id>',
+        resource_class_kwargs={'current_user': api.app.current_user}
+    )
---
+++ views/following.py
@@ -0,0 +1,88 @@
+from flask import Response, request
+from flask_restful import Resource
+from models import Following, User, db
+from views import get_authorized_user_ids
+import json
+
+def get_path():
+    return request.host_url + 'api/posts/'
+
+class FollowingListEndpoint(Resource):
+    def __init__(self, current_user):
+        self.current_user = current_user
+    
+    def get(self):
+        followings = Following.query.filter_by(user_id=self.current_user.id)
+        return Response(json.dumps([following.to_dict_following() for following in followings]), mimetype="application/json", status=200)
+
+    def post(self):
+        # create a new "following" record based on the data posted in the body 
+        body = request.get_json()
+
+        #check invalid user_id format
+        try:
+            new_following = int(body.get('user_id'))
+        except:
+            return Response(json.dumps({"message":"'user_id' is not an int."}), mimetype="application/json",  status=400)      
+
+        #check invalid user_id
+        if new_following >= len(User.query.all()):
+            return Response(json.dumps({"message":"'user_id' does not exist."}), mimetype="application/json",  status=404) 
+
+        # check duplicates
+        followings = Following.query.filter_by(user_id=self.current_user.id).all()
+        followings_ids = [following.following_id for following in followings]
+        if new_following in followings_ids:
+            return Response(json.dumps({"message":"already following user_id"}), mimetype="application/json",  status=400)
+
+        following = Following(
+            user_id=self.current_user.id,
+            following_id=new_following
+        )
+        db.session.add(following)    # issues the insert statement
+        db.session.commit()         # commits the change to the database 
+        return Response(json.dumps(following.to_dict_following()), mimetype="application/json", status=201)
+
+class FollowingDetailEndpoint(Resource):
+    def __init__(self, current_user):
+        self.current_user = current_user
+    
+    def delete(self, id):
+        # delete "following" record where "id"=id
+        print(id)
+        # check invalid id format
+        try:
+            id = int(id)
+        except:
+            return Response(json.dumps({"message":"'id' is not an int."}), mimetype="application/json",  status=400)
+        
+        #check invalid id
+        if id >= len(Following.query.all()):
+            return Response(json.dumps({"message":"'id' does not exist."}), mimetype="application/json",  status=404) 
+
+        #check unauthorized id
+        # gets ids of all the following + self
+        user_ids = get_authorized_user_ids(self.current_user)
+        #print(user_ids)
+        if Following.query.get(id).following_id not in user_ids:
+            return Response(json.dumps({"message":"'id' is not authorized."}), mimetype="application/json",  status=404) 
+
+        following = Following.query.get(id)
+        Following.query.filter_by(id=id).delete()
+        db.session.commit()
+        return Response(json.dumps({"message":"Following id={0} was successfully deleted.".format(id)}), mimetype="application/json", status=200)
+
+
+def initialize_routes(api):
+    api.add_resource(
+        FollowingListEndpoint, 
+        '/api/following', 
+        '/api/following/', 
+        resource_class_kwargs={'current_user': api.app.current_user}
+    )
+    api.add_resource(
+        FollowingDetailEndpoint, 
+        '/api/following/<int:id>', 
+        '/api/following/<int:id>/', 
+        resource_class_kwargs={'current_user': api.app.current_user}
+    )
